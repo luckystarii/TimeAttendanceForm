84930162021314de82dbddf131c083889b6ac8f5
tariposio <bas.khom@hotmail.com>
2018-08-01 18:03:21 +0700

    pass

--- TimeAttendanceForm/TimeAttendanceForm/test_excel.cs
+++ TimeAttendanceForm/TimeAttendanceForm/test_excel.cs
@@ -66,7 +66,7 @@ namespace TimeAttendanceForm
                     //textBox1.Text = tbContainer.Rows[4][0].ToString();
                     //dataGridView1.DataSource = tbContainer;
 
-                    tbContainer = LoadExcelFile(pathName, sheetName, 4, 5);
+                    tbContainer = LoadExcelFile_specific_colunm(pathName, sheetName, 4, 4,2,5,7);
                     dataGridView1.DataSource = tbContainer;
                 }
 
@@ -85,6 +85,7 @@ namespace TimeAttendanceForm
 
         public static DataTable LoadExcelFile(string fileName, string worksheetName, int headerRowNumber, int firstDataRowNumber)
         {
+            
             DataTable dt = new DataTable();
 
             Microsoft.Office.Interop.Excel.Application ExcelApplication = new Microsoft.Office.Interop.Excel.Application();
@@ -175,6 +176,117 @@ namespace TimeAttendanceForm
 
             try { ExcelApplication = null; } catch { }
 
+            return dt;
+        }
+
+        public static DataTable LoadExcelFile_specific_colunm(string fileName, string worksheetName, int headerRowNumber, int firstDataRowNumber,int colDateTime,int colTimeIn,int colTimeOut)
+        {
+            DataTable dt = new DataTable();
+
+            Microsoft.Office.Interop.Excel.Application ExcelApplication = new Microsoft.Office.Interop.Excel.Application();
+
+            Microsoft.Office.Interop.Excel.Workbook ExcelWorkbook = ExcelApplication.Workbooks.Open(fileName, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+
+            Microsoft.Office.Interop.Excel.Worksheet ExcelWorksheet = null;
+
+            string WorksheetName = worksheetName;
+            
+            if (string.IsNullOrWhiteSpace(worksheetName))
+            {
+                WorksheetName = ExcelWorkbook.ActiveSheet.Name;
+
+            }
+
+            ExcelWorksheet = (Microsoft.Office.Interop.Excel.Worksheet)ExcelWorkbook.Worksheets[WorksheetName];
+
+            dt.TableName = WorksheetName;
+
+            Dictionary<string, int> Columns = new Dictionary<string, int>();
+
+            for (int i = 0; i < ExcelWorksheet.UsedRange.Columns.Count; i++)
+            {
+                string ColumnHeading = Convert.ToString(((Microsoft.Office.Interop.Excel.Range)ExcelWorksheet.Cells[headerRowNumber, i + 1]).Value2);
+
+                if (!String.IsNullOrWhiteSpace(ColumnHeading) && !dt.Columns.Contains(ColumnHeading))
+                {
+                    Columns.Add(ColumnHeading, i + 1);
+
+                    dt.Columns.Add(ColumnHeading);
+
+                }
+
+            }
+              for (int i = 0; i < ExcelWorksheet.UsedRange.Rows.Count - firstDataRowNumber + 1; i++)
+            {
+                try
+                {
+                    int ColumnCount = 0;
+
+                    DataRow Row = dt.NewRow();
+
+                    bool RowHasContent = false;
+
+                    foreach (KeyValuePair<string, int> kvp in Columns)
+                    {
+                        //string data = Convert.ToString(((Microsoft.Office.Interop.Excel.Range)ExcelWorksheet.Cells[i + firstDataRowNumber, kvp.Value]).Value2);
+                        string CellContent = null;
+                        if (kvp.Value == colDateTime)
+                        {
+                             //CellContent = Convert.ToString(((Microsoft.Office.Interop.Excel.Range)ExcelWorksheet.Cells[i + firstDataRowNumber, kvp.Value]).Value2);
+                             string sDate = Convert.ToString(((Microsoft.Office.Interop.Excel.Range)ExcelWorksheet.Cells[i + firstDataRowNumber, kvp.Value]).Value2);
+                             
+                             double date = double.Parse(sDate);
+
+                             CellContent = DateTime.FromOADate(date).ToString("dd/MMM/yyyy");
+                        }
+                        else if (kvp.Value == colTimeIn || kvp.Value == colTimeOut)
+                        {
+                            string sDate = Convert.ToString(((Microsoft.Office.Interop.Excel.Range)ExcelWorksheet.Cells[i + firstDataRowNumber, kvp.Value]).Value2);
+
+                            double date = double.Parse(sDate);
+
+                            CellContent = DateTime.FromOADate(date).ToString("hh:mm");
+                        }
+                        else
+                        {
+                             CellContent = Convert.ToString(((Microsoft.Office.Interop.Excel.Range)ExcelWorksheet.Cells[i + firstDataRowNumber, kvp.Value]).Value2);
+
+                        }
+
+                        Row[ColumnCount] = CellContent;
+                        ColumnCount++;
+
+                        if (!string.IsNullOrWhiteSpace(CellContent))
+                        {
+                            RowHasContent = true;
+
+                        }
+
+                    }
+
+                    if (RowHasContent)
+                    {
+                        dt.Rows.Add(Row); ;
+
+                    }
+
+                }
+                catch
+                {
+
+                }
+
+            }
+
+            // Clean up
+
+            try { ExcelWorksheet = null; } catch { }
+
+            try { ExcelWorkbook.Close(); } catch { }
+
+            try { ExcelWorkbook = null; } catch { }
+
+            try { ExcelApplication = null; } catch { }
             return dt;
         }
     }
