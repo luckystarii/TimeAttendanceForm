147b870c164323799a19ac671b9f8d8de058bed3
siwanon <siwanon@csithai.com>
2018-08-01 15:30:56 +0700

    import excel demo

---
+++ TimeAttendanceForm/TimeAttendanceForm/test_excel.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TimeAttendanceForm
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_load = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(12, 12);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(75, 23);
+            this.btn_load.TabIndex = 0;
+            this.btn_load.Text = "loadfile";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(769, 220);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(107, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(650, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_load);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ TimeAttendanceForm/TimeAttendanceForm/test_excel.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Data.OleDb;
+using System.IO;
+
+namespace TimeAttendanceForm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();  //create openfileDialog Object
+                openFileDialog1.Filter = "XML Files (*.xml; *.xls; *.xlsx; *.xlsm; *.xlsb) |*.xml; *.xls; *.xlsx; *.xlsm; *.xlsb";//open file format define Excel Files(.xls)|*.xls| Excel Files(.xlsx)|*.xlsx| 
+                openFileDialog1.FilterIndex = 3;
+
+                openFileDialog1.Multiselect = false;        //not allow multiline selection at the file selection level
+                openFileDialog1.Title = "Open Text File-R13";   //define the name of openfileDialog
+                openFileDialog1.InitialDirectory = @"Desktop"; //define the initial directory
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)        //executing when file open
+                {
+                    string pathName = openFileDialog1.FileName;
+                    string fileName = System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+                    DataTable tbContainer = new DataTable();
+                    string strConn = string.Empty;
+                    string sheetName = "";
+
+                    //FileInfo file = new FileInfo(pathName);
+                    //if (!file.Exists) { throw new Exception("Error, file doesn't exists!"); }
+                    //string extension = file.Extension;
+                    //switch (extension)
+                    //{
+                    //    case ".xls":
+                    //        strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathName + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'";
+                    //        break;
+                    //    case ".xlsx":
+                    //        strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + pathName + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=1;'";
+                    //        break;
+                    //    default:
+                    //        strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathName + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'";
+                    //        break;
+                    //}
+                    //OleDbConnection cnnxls = new OleDbConnection(strConn);
+                    //OleDbDataAdapter oda = new OleDbDataAdapter(string.Format("select * from [{0}$]", sheetName), cnnxls);
+                    //oda.Fill(tbContainer);
+
+                    //textBox1.Text = tbContainer.Rows[4][0].ToString();
+                    //dataGridView1.DataSource = tbContainer;
+
+                    tbContainer = LoadExcelFile(pathName, sheetName, 4, 5);
+                    dataGridView1.DataSource = tbContainer;
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error!");
+            }
+
+        }// end function button1_Click
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        public static DataTable LoadExcelFile(string fileName, string worksheetName, int headerRowNumber, int firstDataRowNumber)
+        {
+            DataTable dt = new DataTable();
+
+            Microsoft.Office.Interop.Excel.Application ExcelApplication = new Microsoft.Office.Interop.Excel.Application();
+
+            Microsoft.Office.Interop.Excel.Workbook ExcelWorkbook = ExcelApplication.Workbooks.Open(fileName, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+
+            Microsoft.Office.Interop.Excel.Worksheet ExcelWorksheet = null;
+
+            string WorksheetName = worksheetName;
+
+            if (string.IsNullOrWhiteSpace(worksheetName))
+            {
+                WorksheetName = ExcelWorkbook.ActiveSheet.Name;
+
+            }
+
+            ExcelWorksheet = (Microsoft.Office.Interop.Excel.Worksheet)ExcelWorkbook.Worksheets[WorksheetName];
+
+            dt.TableName = WorksheetName;
+
+            // Add the columns
+
+            Dictionary<string, int> Columns = new Dictionary<string, int>();
+
+            for (int i = 0; i < ExcelWorksheet.UsedRange.Columns.Count; i++)
+            {
+                string ColumnHeading = Convert.ToString(((Microsoft.Office.Interop.Excel.Range)ExcelWorksheet.Cells[headerRowNumber, i + 1]).Value2);
+
+                if (!String.IsNullOrWhiteSpace(ColumnHeading) && !dt.Columns.Contains(ColumnHeading))
+                {
+                    Columns.Add(ColumnHeading, i + 1);
+
+                    dt.Columns.Add(ColumnHeading);
+
+                }
+
+            }
+
+            // Add the rows
+
+            for (int i = 0; i < ExcelWorksheet.UsedRange.Rows.Count - firstDataRowNumber + 1; i++)
+            {
+                try
+                {
+                    int ColumnCount = 0;
+
+                    DataRow Row = dt.NewRow();
+
+                    bool RowHasContent = false;
+
+                    foreach (KeyValuePair<string, int> kvp in Columns)
+                    {
+                        string CellContent = Convert.ToString(((Microsoft.Office.Interop.Excel.Range)ExcelWorksheet.Cells[i + firstDataRowNumber, kvp.Value]).Value2);
+
+                        Row[ColumnCount] = CellContent;
+
+                        ColumnCount++;
+
+                        if (!string.IsNullOrWhiteSpace(CellContent))
+                        {
+                            RowHasContent = true;
+
+                        }
+
+                    }
+
+                    if (RowHasContent)
+                    {
+                        dt.Rows.Add(Row); ;
+
+                    }
+
+                }
+                catch
+                {
+
+                }
+
+            }
+
+            // Clean up
+
+            try { ExcelWorksheet = null; } catch { }
+
+            try { ExcelWorkbook.Close(); } catch { }
+
+            try { ExcelWorkbook = null; } catch { }
+
+            try { ExcelApplication = null; } catch { }
+
+            return dt;
+        }
+    }
+}
