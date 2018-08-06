a25ed70fadc97bf6ced05e18509212cdbd5e304a
siriwat <siriwat__arm@outlook.com>
2018-08-06 15:31:27 +0700

    set icon

--- TimeAttendanceForm/Test_2/Home.Designer.cs
+++ TimeAttendanceForm/Test_2/Home.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
             this.panel_Import_Export = new System.Windows.Forms.Panel();
             this.btn_Browse_Dest_file = new System.Windows.Forms.Button();
             this.btn_Browse_Target_file = new System.Windows.Forms.Button();
@@ -715,10 +716,10 @@
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.panel1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 32);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(484, 565);
+            this.tabPage2.Size = new System.Drawing.Size(484, 573);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Config";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -747,7 +748,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(478, 559);
+            this.panel1.Size = new System.Drawing.Size(478, 567);
             this.panel1.TabIndex = 0;
             // 
             // label23
@@ -935,14 +936,15 @@
             this.label29.TabIndex = 25;
             this.label29.Text = "Name";
             // 
-            // Form1
+            // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(492, 601);
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Home";
             this.Text = "C.S.I. Group - Time Attendance";
             this.panel_Import_Export.ResumeLayout(false);
             this.panel_Import_Export.PerformLayout();
